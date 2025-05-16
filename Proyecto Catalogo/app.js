9ac28af04c37aa415bbcb135d72d2163850bd672
Santiago Ayala <santi910xd@gmail.com>
2025-05-15 21:18:34 -0500

    Sube todos los cambios realizados al proyecto hasta el 16 de mayo

--- Proyecto Catalogo/app.js
+++ Proyecto Catalogo/app.js
@@ -5,6 +5,64 @@ const contenedorCategorias = document.getElementById("categorias");
 let Productos = [];
 let categoriasSeleccionada = "all";
 
+//LOGICA LOGIN
+document.addEventListener("DOMContentLoaded", () => {
+
+    const loginForm = document.getElementById("loginForm");
+
+    if(loginForm){
+        loginForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
+
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            const mensaje = document.getElementById("mensaje");
+            try {
+                const response = await fetch("https://fakestoreapi.com/auth/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        username,
+                        password,
+                    }),
+                });
+
+                if (!response.ok) {
+                    throw new Error("Error en la respuesta de la API");
+                }
+
+                const data = await response.json();
+                localStorage.setItem("token", data.token);
+                mensaje.textContent = "Inicio de sesión exitoso";
+                mensaje.classList.add("text-green-500");
+
+                setTimeout(() => {
+                    window.location.href = "index.html"; // Redirigir a la página principal
+                }, 1500);
+                
+            } catch (error) {
+                console.error("Error al iniciar sesión:", error);
+                mensaje.textContent = "Error al iniciar sesión. Verifica tus credenciales.";                
+                mensaje.classList.add("text-red-500");
+            }
+        });
+    } if (contenedorProductos && contenedorCategorias && inputBusqueda) {
+    // Si los elementos existen, se ejecuta la lógica de productos
+    cargarProductos();
+    cargarCategorias();
+
+    //Agregar evento de búsqueda
+    inputBusqueda.addEventListener("input", filtrarProductos);
+    }
+});
+
+
+
+
+
+//LOGICA DE PRODUCTOS
 async function cargarProductos() {
     try {
         const respuesta = await fetch("https://fakestoreapi.com/products");
